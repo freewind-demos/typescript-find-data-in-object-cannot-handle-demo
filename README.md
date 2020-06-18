b004c395b3cf9bd93667bb670893d3146b7ca178
Freewind <nowindlee@gmail.com>
2020-06-18 18:51:26 +0800

    typescript-find-data-in-object-cannot-handle-demo

--- README.md
+++ README.md
@@ -1,18 +1,22 @@
-TypeScript Hello World Demo
+TypeScript Find Data in Object Cannot Handle Demo
 ===========================
+
+比较两个object，一个包括的内容都是正确的，另一个包括额外的值。
+
+如何通过比较，找到那些“额外”的值。
 
 ```
 npm install
 npm run demo
 ```
 
-Note:
-
-`esModuleInterop` is recommended to set to `true`,
-since we can have consistent importing syntax with babel,
-always use:
+打印结果如下： 
 
 ```
-import some from 'some'
+### unhandledFields [
+  { fieldPath: 'root.aaa', value: 222 },
+  { fieldPath: 'root.bbb[1]', value: 333 },
+  { fieldPath: 'root.ccc.c33', value: 33 },
+  { fieldPath: 'root.ddd[0]', value: { bbb: 111 } }
+]
 ```
-
--- hello.ts
+++ hello.ts
@@ -1,1 +1,89 @@
-console.log('Hello');
+import isEqual from 'lodash/isEqual';
+
+type InvalidField = {
+  fieldPath: string,
+  value: any
+}
+
+function diffField(
+  {internalFieldPath, internalValue}: { internalFieldPath: string, internalValue: any },
+  {externalFieldPath, externalValue}: { externalFieldPath: string, externalValue: any },
+  result: InvalidField[]
+) {
+  if (externalValue === internalValue) {
+    return;
+  }
+
+  // should check array first
+  if (Array.isArray(externalValue) && Array.isArray(internalValue)) {
+    externalValue.forEach((externalItem, index) => {
+      if (!internalValue.some(internalItem => isEqual(internalItem, externalItem))) {
+        result.push({
+          fieldPath: `${externalFieldPath}[${index}]`,
+          value: externalItem
+        });
+      }
+    })
+    return;
+  }
+
+  // object
+  if (typeof externalValue === 'object' && typeof internalValue === 'object') {
+    for (const externalKey in externalValue) {
+      diffField({
+        internalFieldPath: `${internalFieldPath}.${externalKey}`,
+        internalValue: internalValue[externalKey]
+      }, {
+        externalFieldPath: `${externalFieldPath}.${externalKey}`,
+        externalValue: externalValue[externalKey]
+      }, result);
+    }
+    return;
+  }
+
+
+  result.push({
+    fieldPath: externalFieldPath,
+    value: externalValue
+  });
+}
+
+function findUnhandledFields(internal: { [key: string]: any }, external: { [key: string]: any }): InvalidField[] {
+  const result: InvalidField[] = [];
+  diffField({
+    internalFieldPath: 'root',
+    internalValue: internal,
+  }, {
+    externalFieldPath: 'root',
+    externalValue: external
+  }, result);
+  return result;
+}
+
+const internal = {
+  aaa: 111,
+  bbb: [111, 222],
+  ccc: {
+    c11: 11,
+    c22: 22
+  },
+  ddd: [
+    {aaa: 111},
+    {aaa: 222}
+  ]
+}
+const external = {
+  aaa: 222,
+  bbb: [222, 333],
+  ccc: {
+    c11: 11,
+    c33: 33
+  },
+  ddd: [
+    {bbb: 111},
+    {aaa: 222}
+  ]
+}
+
+const unhandledFields = findUnhandledFields(internal, external)
+console.log('### unhandledFields', unhandledFields);
